5c4a488106ddca3fcec225dd5096e6c74f8cbc92
Jeffrey Emekwue <jemekwu1@my.westga.edu>
2022-10-31 04:02:37 -0400

    Simultaneous multi-directional Player movement, and Color Swap.

--- Model/PlayerDotManager.cs
+++ Model/PlayerDotManager.cs
@@ -1,4 +1,5 @@
-﻿using Windows.System;
+﻿using System;
+using Windows.System;
 using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -21,6 +22,11 @@ namespace DodgeDots.Model
         private readonly Canvas backgroundCanvas;
         private readonly Color[] colors;
         private int outsideColorIndex;
+        private bool isUpKeyDown;
+        private bool isDownKeyDown;
+        private bool isLeftKeyDown;
+        private bool isRightKeyDown;
+        private bool isSpaceKeyDown;
 
         #endregion
 
@@ -48,7 +54,15 @@ namespace DodgeDots.Model
             this.backgroundCanvas = background;
             background.Children.Add(this.PlayerDot.Sprite);
             this.placePlayerCenteredInGameArena();
-            Window.Current.CoreWindow.KeyDown += this.CoreWindow_KeyDown;
+            
+            Window.Current.CoreWindow.KeyDown += this.playerKeyDownFlag;
+            Window.Current.CoreWindow.KeyUp += this.playerKeyUpFlag;
+            
+            var playerTimer = new DispatcherTimer();
+            playerTimer.Tick += this.PlayerTimer_Tick;
+            playerTimer.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            playerTimer.Start();
+            
             this.colors = new[]
             {
                 GameSettings.PrimaryDotColor, GameSettings.SecondaryDotColor
@@ -59,10 +73,84 @@ namespace DodgeDots.Model
 
         #region Methods
 
-        private void CoreWindow_KeyDown(CoreWindow sender, KeyEventArgs args)
+        private void PlayerTimer_Tick(object sender, object e)
         {
-            this.playerKeyDownMovement(args);
-            if (args.VirtualKey == VirtualKey.Space && this.ColorSwapLevel == 1)
+            if (this.isLeftKeyDown && this.PlayerDot.X - this.PlayerDot.SpeedX >= 0)
+            {
+                this.PlayerDot.MoveLeft();
+            }
+
+            if (this.isRightKeyDown && this.PlayerDot.X + this.PlayerDot.SpeedX <=
+                this.backgroundCanvas.Width - this.PlayerDot.Width)
+            {
+                this.PlayerDot.MoveRight();
+            }
+
+            if (this.isUpKeyDown && this.PlayerDot.Y - this.PlayerDot.SpeedY >= 0)
+            {
+                this.PlayerDot.MoveUp();
+            }
+
+            if (this.isDownKeyDown && this.PlayerDot.Y + this.PlayerDot.SpeedY <=
+                this.backgroundCanvas.Height - this.PlayerDot.Height)
+            {
+                this.PlayerDot.MoveDown();
+            }
+
+            if (this.isSpaceKeyDown)
+            {
+                this.colorSwap();
+                this.isSpaceKeyDown = false;
+            }
+        }
+
+        private void playerKeyDownFlag(CoreWindow sender, KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    this.isLeftKeyDown = true;
+                    break;
+                case VirtualKey.Right:
+                    this.isRightKeyDown = true;
+                    break;
+                case VirtualKey.Up:
+                    this.isUpKeyDown = true;
+                    break;
+                case VirtualKey.Down:
+                    this.isDownKeyDown = true;
+                    break;
+                case VirtualKey.Space:
+                    this.isSpaceKeyDown = true;
+                    break;
+            }
+        }
+
+        private void playerKeyUpFlag(CoreWindow sender, KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    this.isLeftKeyDown = false;
+                    break;
+                case VirtualKey.Right:
+                    this.isRightKeyDown = false;
+                    break;
+                case VirtualKey.Up:
+                    this.isUpKeyDown = false;
+                    break;
+                case VirtualKey.Down:
+                    this.isDownKeyDown = false;
+                    break;
+                case VirtualKey.Space:
+                    this.isSpaceKeyDown = false;
+                    break;
+            }
+        }
+
+        private void colorSwap()
+        {
+            if (this.ColorSwapLevel == 1)
             {
                 switch (this.outsideColorIndex)
                 {
@@ -77,42 +165,6 @@ namespace DodgeDots.Model
                         this.outsideColorIndex = 0;
                         break;
                 }
-            }
-        }
-
-        private void playerKeyDownMovement(KeyEventArgs args)
-        {
-            switch (args.VirtualKey)
-            {
-                case VirtualKey.Left:
-                    if (this.PlayerDot.X - this.PlayerDot.SpeedX >= 0)
-                    {
-                        this.PlayerDot.MoveLeft();
-                    }
-
-                    break;
-                case VirtualKey.Right:
-                    if (this.PlayerDot.X + this.PlayerDot.SpeedX <= this.backgroundCanvas.Width - this.PlayerDot.Width)
-                    {
-                        this.PlayerDot.MoveRight();
-                    }
-
-                    break;
-                case VirtualKey.Up:
-                    if (this.PlayerDot.Y - this.PlayerDot.SpeedY >= 0)
-                    {
-                        this.PlayerDot.MoveUp();
-                    }
-
-                    break;
-                case VirtualKey.Down:
-                    if (this.PlayerDot.Y + this.PlayerDot.SpeedY <=
-                        this.backgroundCanvas.Height - this.PlayerDot.Height)
-                    {
-                        this.PlayerDot.MoveDown();
-                    }
-
-                    break;
             }
         }
 
